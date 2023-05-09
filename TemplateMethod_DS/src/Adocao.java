e9e0bef719a6ffd76a2d1e24ea2b2223c4680345
Malau172 <darosalaura83@gmail.com>
2023-05-09 17:06:50 -0300

    trabalho

---
+++ TemplateMethod_DS/src/Adocao.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public class Adocao {
+  private Date dataAdocao;
+  private int idPet;
+  private int idAdotante;
+
+
+  public Adocao(Date dataAdocao, int idPet, int idAdotante) {
+    this.dataAdocao = dataAdocao;
+    this.idPet = idPet;
+    this.idAdotante = idAdotante;
+  }
+   
+
+  public void cadastraAdocao() {
+    System.out.println("Adoção cadastrado com sucesso!");
+  }
+
+  public void editaAdocao(Date dataAdocao, int idPet, int idAdotante) {
+    this.dataAdocao = dataAdocao;
+    this.idPet = idPet;
+    this.idAdotante = idAdotante;
+    
+      System.out.println("Adoção editada com sucesso!");
+  }
+
+  public void removeAdotante() {
+    System.out.println("Adotante removido com sucesso!");
+  }
+
+  
+}
---
+++ TemplateMethod_DS/src/EnderecoAdotante.java
@@ -0,0 +1,45 @@
+public class EnderecoAdotante {
+  private String nome;
+  private String sobrenome;
+  private String contato;
+  private String email;
+  private String complemento;
+  private String telefone;
+  private int idEndereco;
+  
+  
+  public Adotante(String nome, String sobrenome, String email, int idEndereco, String contato, String telefone) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.contato = contato;
+    this.email = email;
+    this.complemento = complemento;
+    this.telefone = telefone;
+    this.idEndereco = idEndereco;
+
+  }
+  
+  public void cadastraAdotante() {
+    System.out.println("Adotante cadastrado com sucesso!");
+  }
+  
+  public void editaAdotante(String nome, String sobrenome, String email, int idEndereco, String contato, String telefone) {
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.contato = contato;
+    this.email = email;
+    this.complemento = complemento;
+    this.telefone = telefone;
+    this.idEndereco = idEndereco;
+  
+    System.out.println("Adotante editado com sucesso!");
+  }
+  
+  public void removeAdotante() {
+    System.out.println("Adotante removido com sucesso!");
+  }
+  
+
+  
+  
+}
